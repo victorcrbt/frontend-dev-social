a85ff9cd62a070ad4f9db680255ff9b0307fe2dc
Victor Batalha <madivcb@gmail.com>
2019-09-08 12:40:31 -0300

    feat: global styles setup

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './config/ReactotronConfig';
 import history from './services/history';
 
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+
+      <GlobalStyles />
     </Router>
   );
 }
---
+++ src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  body {
+    width: 100%;
+    -webkit-font-smoothing: antialiased;
+    background: #272727;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto', Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 14px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
