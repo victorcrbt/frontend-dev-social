1e2f3269a0c916b9f00f991f58208538bce8b102
Victor Batalha <madivcb@gmail.com>
2019-09-09 00:01:43 -0300

    feat: auth persistance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 import history from './services/history';
-import store from './store';
+import { store, persistor } from './store';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
@@ -12,11 +13,13 @@ import GlobalStyles from './styles/global';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
 
-        <GlobalStyles />
-      </Router>
+          <GlobalStyles />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
