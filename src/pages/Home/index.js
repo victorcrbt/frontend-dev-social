b89b2003178fc114a7a4a16524cccecc66c105dd
Victor Batalha <madivcb@gmail.com>
2019-09-10 15:45:09 -0300

    feat: set headers on login or page refresh

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Home() {
+  useEffect(() => {
+    async function load() {
+      try {
+        const response = await api.get('/users');
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    load();
+  }, []);
+
   return <h1>Home</h1>;
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -15,6 +15,8 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
     toast.success('Login realizado com sucesso!');
   } catch (err) {
@@ -66,7 +68,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
