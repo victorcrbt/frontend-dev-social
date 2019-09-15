59fd2dec78a9d8d17bd8e9c4f0fe1dd6dcf53d54
Victor Batalha <madivcb@gmail.com>
2019-09-14 21:22:18 -0300

    feat: like and dislike post

--- src/pages/Home/Posts/PostInteractions/index.js
+++ src/pages/Home/Posts/PostInteractions/index.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
-import { Container, Likes, Comments } from './styles';
+import api from '~/services/api';
 
-export default function PostInteractions({ likes, comments }) {
-  const [isEmpty, setIsEmpty] = useState(true);
+import { Container, Interactions, Likes, Comments, Actions } from './styles';
+
+export default function PostInteractions({ likes, comments, postId }) {
+  const profile = useSelector(state => state.user.profile);
+
+  const [isEmpty, setIsEmpty] = useState(false);
   const [likesList, setLikesList] = useState([]);
   const [commentsList, setCommentsList] = useState([]);
+  const [liked, setLiked] = useState(false);
 
   useEffect(() => {
-    if (likesList.length > 0 || commentsList.length > 0)
-      return setIsEmpty(false);
+    if (likesList.length === 0 && commentsList.length === 0) {
+      return setIsEmpty(true);
+    }
+    return setIsEmpty(false);
   }, [likesList, commentsList]);
 
   useEffect(() => {
@@ -17,23 +27,87 @@ export default function PostInteractions({ likes, comments }) {
     setCommentsList(comments);
   }, [likes, comments]);
 
-  return (
-    <Container isEmpty={isEmpty}>
-      {likesList && likesList.length !== 0 && (
-        <Likes>
-          {likesList.length === 1
-            ? `1 curtida`
-            : `${likesList.length} curtidas`}
-        </Likes>
-      )}
+  useEffect(() => {
+    function loadLikes() {
+      const userAlreadyLiked = likes.findIndex(like => like === profile.id);
 
-      {commentsList && commentsList.length !== 0 && (
-        <Comments>
-          {commentsList.length === 1
-            ? `1 comentario`
-            : `${commentsList.length} comentarios`}
-        </Comments>
-      )}
+      if (userAlreadyLiked === -1) return;
+
+      return setLiked(true);
+    }
+
+    loadLikes();
+  }, [likes, profile.id]);
+
+  async function handleLike(post_id) {
+    try {
+      await api.post(`/posts/${post_id}/like`);
+
+      setLikesList([...likesList, profile.id]);
+      setLiked(true);
+    } catch (err) {
+      toast.error(err.response.data.error);
+    }
+  }
+
+  async function handleDislike(post_id) {
+    try {
+      await api.delete(`/posts/${post_id}/dislike`);
+
+      const updatedLikes = likesList;
+
+      const likeIndex = updatedLikes.findIndex(like => like === profile.id);
+
+      updatedLikes.splice(likeIndex, 1);
+
+      setLikesList([...updatedLikes]);
+      setLiked(false);
+    } catch (err) {
+      toast.error(err.response.data.error);
+    }
+  }
+
+  return (
+    <Container>
+      <Interactions isEmpty={isEmpty}>
+        {likesList && likesList.length !== 0 && (
+          <Likes>
+            {likesList.length === 1
+              ? `1 curtida`
+              : `${likesList.length} curtidas`}
+          </Likes>
+        )}
+
+        {commentsList && commentsList.length !== 0 && (
+          <Comments>
+            {commentsList.length === 1
+              ? `1 comentario`
+              : `${commentsList.length} comentarios`}
+          </Comments>
+        )}
+      </Interactions>
+
+      <Actions liked={liked}>
+        {liked ? (
+          <>
+            <button
+              id="dislike"
+              type="button"
+              onClick={() => handleDislike(postId)}
+            >
+              <MdFavorite size={20} />
+              <span>Descurtir</span>
+            </button>
+          </>
+        ) : (
+          <>
+            <button id="like" type="button" onClick={() => handleLike(postId)}>
+              <MdFavoriteBorder size={20} />
+              <span>Curtir</span>
+            </button>
+          </>
+        )}
+      </Actions>
     </Container>
   );
 }
--- src/pages/Home/Posts/PostInteractions/styles.js
+++ src/pages/Home/Posts/PostInteractions/styles.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const Interactions = styled.div`
   padding: 10px 5px;
+  padding: ${props => (props.isEmpty ? 0 : '10px 5px')};
   margin-bottom: 10px;
 
   border-top: 1px solid #ccc;
@@ -21,4 +24,31 @@ export const Comments = styled.div`
 
   font-weight: bold;
   color: #666;
+`;
+
+export const Actions = styled.div`
+  padding: 0px 5px;
+  margin-bottom: 10px;
+
+  display: flex;
+
+  svg {
+    color: ${({ liked }) => (liked ? '#BC40F4' : '#444')};
+  }
+
+  button {
+    border: 0;
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+  }
+
+  span {
+    margin-left: 5px;
+
+    color: ${({ liked }) => (liked ? '#BC40F4' : '#444')};
+    font-weight: bold;
+    cursor: pointer;
+  }
 `;
