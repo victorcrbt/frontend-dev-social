fc3fadd9caed4f4435e73c97bd6cc16356a94c1b
Victor Batalha <madivcb@gmail.com>
2019-09-22 20:39:41 -0300

    feat: add loading animations

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Container } from './styles';
+import { Container, Loading } from './styles';
 
-export default function Button({ children, ...rest }) {
-  return <Container {...rest}>{children}</Container>;
+export default function Button({
+  children,
+  loading,
+  iconWidth,
+  iconHeight,
+  ...rest
+}) {
+  return (
+    <Container {...rest}>
+      {loading ? <Loading width={iconWidth} height={iconHeight} /> : children}
+    </Container>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf()])
     .isRequired,
+  loading: PropTypes.bool,
+  iconWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  iconHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+Button.defaultProps = {
+  loading: true,
+  iconWidth: 40,
+  iconHeight: 40,
 };
